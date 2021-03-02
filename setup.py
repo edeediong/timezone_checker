376d4d05ad0b5f9eb40acc126abd0132dc39bbb6
Edidiong Etuk <edidiongmichael08@gmail.com>
2021-03-02 22:35:48 +0100

    Changed to version 5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="timechecker",
-    version="0.0.4",
+    version="0.0.5",
     author="Edidiong Etuk",
     author_email="edeediong@gmail.com",
     url="https://bit.ly/edeediong-resume",
